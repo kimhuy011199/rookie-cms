7b6a575ee3ea6f61886b37791ca663d60aa2f0e4
kimhuy011199 <kimhuy011199@gmail.com>
2022-06-19 13:55:23 +0700

    Implement content item component

---
+++ src/shared/components/ContentItem/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './style.module.css';
+
+interface ContentItemProps {
+  item: any;
+  type: string;
+}
+
+const ContentItem = (props: ContentItemProps) => {
+  const { item, type } = props;
+  const linkTo = `/${type}/${item._id}`;
+
+  return (
+    <Link to={linkTo} className={style.contentItem}>
+      <span></span>
+      <span>{item._id}</span>
+      <span className={style.title}>{item.title}</span>
+    </Link>
+  );
+};
+
+export default ContentItem;
