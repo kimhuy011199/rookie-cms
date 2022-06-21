5a4198c76a7da03b603bf451a1a510a93751698b
kimhuy011199 <kimhuy011199@gmail.com>
2022-06-21 23:14:45 +0700

    Add no number to content item

--- src/shared/components/ContentItem/index.tsx
+++ src/shared/components/ContentItem/index.tsx
@@ -5,15 +5,16 @@ import style from './style.module.css';
 interface ContentItemProps {
   item: any;
   type: string;
+  noNumber: number;
 }
 
 const ContentItem = (props: ContentItemProps) => {
-  const { item, type } = props;
+  const { item, type, noNumber } = props;
   const linkTo = `/${type}/${item._id}`;
 
   return (
     <Link to={linkTo} className={style.contentItem}>
-      <span></span>
+      <span>{noNumber}</span>
       <span>{item._id}</span>
       <span className={style.title}>
         {item.title || item.name || item.content || item.displayName}
