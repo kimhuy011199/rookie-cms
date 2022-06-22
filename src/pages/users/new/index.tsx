9c729d2f76d226d68d24a158a24677750bc12398
kimhuy011199 <kimhuy011199@gmail.com>
2022-06-22 11:15:25 +0700

    Implement new user page

---
+++ src/pages/users/new/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import style from './style.module.css';
+import { userType } from '../../../stores/users/userType';
+import AnswerForm from '../../../shared/components/AnswerForm';
+import { createUser } from '../../../stores/users/userSlice';
+
+const NewUser = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { isSuccess, isError } = useSelector((state: any) => state.users);
+
+  const submitForm = (data: any) => {
+    console.log({ data });
+    // dispatch(createUser(data));
+  };
+
+  useEffect(() => {
+    if (isSuccess === userType.CREATE_USER) {
+      toast(t('toast.create_answer_success'));
+      navigate('/answers');
+    }
+  }, [isSuccess, navigate, t]);
+
+  useEffect(() => {
+    if (isError === userType.CREATE_USER) {
+      toast(t('toast.unsuccess'));
+    }
+  }, [isError, t]);
+
+  return (
+    <div className={style.container}>
+      <h2 className={style.heading}>{t('answers.create_new_answer')}</h2>
+      <AnswerForm submitFunc={submitForm} />
+    </div>
+  );
+};
+
+export default NewUser;
